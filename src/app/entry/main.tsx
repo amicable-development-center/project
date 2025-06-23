0c584811df5d4ad130224533e2f01ab00d14a970
Taegwan Youn <tkyoun0421@naver.com>
2025-06-24 00:49:57 +0900

    chore: queryClient 제작

--- src/app/entry/main.tsx
+++ src/app/entry/main.tsx
@@ -1,10 +1,15 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "@app/routes/App";
 
+import queryClient from "@shared/react-query/queryClient";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
