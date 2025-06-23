a61a6720ad6c4f2d34300af7a17280da4a64c040
윤다빈 <116141225+czmcm5@users.noreply.github.com>
2025-06-24 01:49:25 +0900

    chore: 함수 return 타입 추가

--- src/entities/project/project.ts
+++ src/entities/project/project.ts
@@ -8,7 +8,7 @@ import {
 
 import { db } from "@shared/firebase/firebase";
 
-export async function getComments() {
+export async function getComments(): Promise<null | {}> {
   const docRef = doc(db, "coments", "test");
   const docSnap = await getDoc(docRef);
 
@@ -20,13 +20,13 @@ export async function getComments() {
   return docSnap.data();
 }
 
-export async function updateTitle(newTitle: string) {
+export async function updateTitle(newTitle: string): Promise<void> {
   const docRef = doc(db, "coments", "test");
   await setDoc(docRef, { title: newTitle }, { merge: true });
   console.log("문서가 새로 작성되거나 덮어쓰기 됐어요.");
 }
 
-export async function addPost(title: string) {
+export async function addPost(title: string): Promise<string> {
   const postsRef = collection(db, "posts");
   const docRef = await addDoc(postsRef, {
     title,
