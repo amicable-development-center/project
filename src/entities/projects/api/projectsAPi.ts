7448603e2af85be6c4a5b2479150c22ef205325f
윤다빈 <116141225+czmcm5@users.noreply.github.com>
2025-06-24 14:15:08 +0900

    feat: getProjectList, insertProjectItem api 추가
    getProjectList: 메인홈에서 프로젝트 목록을 불러옵니다.
    insertProjectItem: 등록 페이지에서 프로젝트를 등록합니다.

---
+++ src/entities/projects/api/projectsAPi.ts
@@ -0,0 +1,23 @@
+import { collection, getDocs } from "firebase/firestore/lite";
+
+import type { ProjectListRes } from "@entities/types/projects";
+
+import { db } from "@shared/firebase/firebase";
+
+/** firebase project 목록 불러오기 */
+export async function getProjectList(): Promise<ProjectListRes[]> {
+  try {
+    const listRef = collection(db, "projects");
+    const querySnapshot = await getDocs(listRef);
+
+    const posts = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    return posts as ProjectListRes[];
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+}
---
+++ src/features/projects/projdectsApi.ts
@@ -0,0 +1,34 @@
+import { addDoc, collection, doc, setDoc } from "firebase/firestore/lite";
+
+import type { ProjectItemInsertReq } from "@entities/types/projects";
+
+import { db } from "@shared/firebase/firebase";
+
+/** firebase projects에 item 등록 */
+export async function insertProjectItem(
+  projectItem: ProjectItemInsertReq
+): Promise<{ success: boolean; message: string; id?: string }> {
+  try {
+    const postsRef = collection(db, "projects");
+    const docRef = await addDoc(postsRef, projectItem);
+
+    return {
+      success: true,
+      message: "프로젝트가 성공적으로 등록되었습니다.",
+      id: docRef.id,
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      success: false,
+      message: "프로젝트 등록에 실패하였습니다.",
+    };
+  }
+}
+
+/** firebase projects에 item 수정 */
+export async function updateProjectItem(): Promise<void> {
+  return;
+  const docRef = doc(db, "coments", "test");
+  await setDoc(docRef, { title: "" }, { merge: true });
+}
