4f036bafd095111c0568cdf364743d134fd637c1
Sara han <hans63237@gmail.com>
2025-06-24 23:11:18 +0900

    refactor: signUp 훅 userInput 타입 적용 및 코드 정리

---
+++ src/features/user/hooks/userSignUp.ts
@@ -0,0 +1,33 @@
+import { useNavigate } from "react-router-dom";
+
+import { useAuthStore } from "@shared/stores/authStore";
+import type { UserInput } from "@shared/user/types/user";
+
+import { saveUser } from "../api/userApi";
+
+export const useSignUp = () => {
+  const navigate = useNavigate();
+  const user = useAuthStore((state) => state.user);
+
+  const signUp = async (userInput: UserInput) => {
+    if (!user) return;
+
+    const { name, userRole, experience, introduceMyself } = userInput;
+    console.log("서브밋 데이터:", userInput);
+    await saveUser(user.uid, {
+      id: user.uid,
+      name,
+      userRole,
+      experience,
+      email: user.email ?? "",
+      avatar: user.photoURL || "",
+      likeProjects: [],
+      appliedProjects: [],
+      introduceMyself,
+    });
+
+    navigate("/");
+  };
+
+  return { signUp };
+};
