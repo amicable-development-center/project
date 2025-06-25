b3577fcd9d8ca27d422faf1428a17728ec686fde
윤다빈 <116141225+czmcm5@users.noreply.github.com>
2025-06-25 22:51:18 +0900

    feat: 작성자라면 삭제 가능하도록 버튼 추가

--- src/features/projects/queries/useProjectDelete.ts
+++ src/features/projects/queries/useProjectDelete.ts
@@ -5,27 +5,34 @@ import { deleteProjectItem } from "@features/projects/api/projectsApi";
 
 import type { ApiResMessage } from "@entities/projects/types/firebase";
 
+import { useAuthStore } from "@shared/stores/authStore";
+
 const useProjectDelete = (): UseMutationResult<
   ApiResMessage,
   Error,
   string
 > => {
   const Navigate = useNavigate();
+  const user = useAuthStore((state) => state.user);
 
   return useMutation({
     mutationFn: (id: string) => {
+      if (user?.uid !== id) {
+        throw new Error("삭제 권한이 없습니다.");
+      }
       return deleteProjectItem(id);
     },
     onSuccess: (data) => {
       if (data.message) {
         alert(data.message);
       }
+      // 게시글 삭제 후 목록페이지로 이동
       if (data.success) {
-        Navigate("/");
+        Navigate("/project");
       }
     },
     onError: (err) => {
-      alert("정상적으로 삭제되지 않았습니다.");
+      alert(err || "정상적으로 삭제되지 않았습니다.");
       console.log(err);
     },
   });
---
+++ src/features/projects/ui/ProjectDelete.tsx
@@ -0,0 +1,41 @@
+import { Box, styled, Typography } from "@mui/material";
+import type { JSX } from "react";
+import { useParams } from "react-router-dom";
+
+import useProjectDelete from "@features/projects/queries/useProjectDelete";
+
+const ProjectDelete = (): JSX.Element => {
+  const { id } = useParams();
+  const { mutate: projectdelete, isPending } = useProjectDelete();
+
+  const handleDeleteBtn = (): void => {
+    if (id && !isPending) {
+      projectdelete(id);
+    }
+  };
+
+  return (
+    <MessageBtn onClick={handleDeleteBtn}>
+      <Typography>삭제</Typography>
+    </MessageBtn>
+  );
+};
+
+export default ProjectDelete;
+
+const MessageBtn = styled(Box)`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 4rem;
+  border-radius: 4px;
+  color: white;
+  background-color: tomato;
+  transition: background-color 0.3s ease;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #e14b30;
+  }
+`;
